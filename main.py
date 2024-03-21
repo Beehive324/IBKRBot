8ffde07616b939b0c1ff1d3ce0abe660b257ffb4
Fairson Soares <Fairson123@hotmail.com>
2024-03-21 22:49:56 +0000

    IBKR Connection

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import time
 from ibapi.client import EClient
+from ibapi.common import TickerId
 from ibapi.wrapper import EWrapper
-import ibapi
+from ibapi.contract import Contract
+from ibapi.order import Order
+import threading
+
 
 # class for Interactive brokers connection
 
@@ -9,15 +14,40 @@ class IbConn(EWrapper, EClient):
     def __init__(self):
         EClient.__init__(self, self)
 
+    # Listening for realtime bars
+    def realtimeBar(self, reqId, time, open_, high, low, close, volume, wap, count):
+        bot.on_bar_update(reqId, time, open_, high, low, close, volume, wap, count)
+
 
 class Bot:
     ib = None
-    #Connect to IB
+
+    # Connect to IB
 
     def __init__(self):
-        ib = IbConn()
-        ib.connect("127.0.0.1", 7497,2)
-        ib.run()
+        self.ib = IbConn()
+        self.ib.connect("127.0.0.1", 7497, 2)
+        ib_thread = threading.Thread(target=self.bot_loop(), daemon=True)
+        ib_thread.start()
+        time.sleep(1)
+        symbol = input("Enter the symbol you want to trade: ")
+        # Creating IB Contract Objects
+        contract = Contract()
+        contract.symbol = symbol.upper()
+        contract.secType = "STK"
+        contract.exchange = "SMART"
+        contract.currency = "USD"
+        # Request real-time market data
+        self.ib.reqRealTimeBars(0, contract, 5, "TRADES", True, [])
+
+    # separate run method to separate thread
+    def bot_loop(self):
+        self.ib.run()
+
+    # Pass realtime bar data to bot objects
+    @staticmethod
+    def on_bar_update(reqId, time, open_, high, low, close, volume, wap, count):
+        print(reqId)
 
 
 bot = Bot()
