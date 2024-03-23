23e423d9de4210ca9fe0a3f2f33736ccc490667d
Fairson Soares <Fairson123@hotmail.com>
2024-03-23 13:13:38 +0000

    Create Contract + Order + Place Order Method

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from ibapi.wrapper import EWrapper
 from ibapi.contract import Contract
 from ibapi.order import Order
 import threading
+import random
 
 
 # class for Interactive brokers connection
@@ -16,17 +17,27 @@ class IbConn(EWrapper, EClient):
 
     # Listening for realtime bars
     def realtimeBar(self, reqId, time, open_, high, low, close, volume, wap, count):
-        bot.on_bar_update(reqId, time, open_, high, low, close, volume, wap, count)
+        super().realtimeBar(reqId, time, open_, high, low, close, volume, wap, count)
+        try:
+            bot.on_bar_update(reqId, time, open_, high, low, close, volume, wap, count)
+        except Exception as e:
+            print(e)
+
+    def error(self, id , error, error_code):
+        print(id)
+        print(error)
+        print(error_code)
 
 
 class Bot:
     ib = None
 
     # Connect to IB
-
     def __init__(self):
+        client_id = random.randint(234,234234)
         self.ib = IbConn()
-        self.ib.connect("127.0.0.1", 7497, 2)
+        self.ib.connect("127.0.0.1", 7497, clientId=client_id)
+        # separate run method to separate thread
         ib_thread = threading.Thread(target=self.bot_loop(), daemon=True)
         ib_thread.start()
         time.sleep(1)
@@ -39,6 +50,28 @@ class Bot:
         contract.currency = "USD"
         # Request real-time market data
         self.ib.reqRealTimeBars(0, contract, 5, "TRADES", True, [])
+
+        #create order object
+        order = Order()
+        order.orderType = "MKT"
+        order.action = "SELL"
+        quantity = 1
+        order.totalQuantity = quantity
+
+        #create Contract object
+        contract = Contract()
+        contract.symbol = symbol
+        contract.exchange = "SMART"
+        contract.secType = "CASH" #forex
+        contract.primaryExchange = "ISLAND"
+        contract.currency = "USD"
+
+        #Place the order
+        order_id = random.randint(0,9000)
+        self.ib.placeOrder(order_id, contract, order)
+
+
+
 
     # separate run method to separate thread
     def bot_loop(self):
