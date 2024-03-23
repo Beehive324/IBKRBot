b0167ccf419842df89c914a0427981a733044b45
Fairson Soares <Fairson123@hotmail.com>
2024-03-23 13:44:32 +0000

    further changes + bar Class

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
 import time
+import datetime
 from ibapi.client import EClient
-from ibapi.common import TickerId
+from ibapi.common import TickerId, BarData
 from ibapi.wrapper import EWrapper
 from ibapi.contract import Contract
 from ibapi.order import Order
+import ta #technical analysis library
+import numpy
+import pandas as p
+import pytz
+import math
 import threading
 import random
+
+#Strategy: buy when price breaks lower highs and higher lows and breaks the RSMA
 
 
 # class for Interactive brokers connection
 
-
+order_id = 1
 class IbConn(EWrapper, EClient):
     def __init__(self):
         EClient.__init__(self, self)
+
+    def historicalData(self, reqId, bar):
+        try:
+            bot.on_bar_update(reqId, bar, False)
+        except Exception as e:
+            print(e)
+    def historicalDataUpdate(self, reqId, bar):
+        try:
+            bot.on_bar_update(reqId, bar, True)
+        except Exception as e:
+            print(e)
+
+    #end of historical data
+    def historicalDataEnd(self, reqId, start, end):
+        print(reqId)
+
+    def nextValidId(self, nextorderId):
+        global order_id
+        order_id = nextorderId
+
 
     # Listening for realtime bars
     def realtimeBar(self, reqId, time, open_, high, low, close, volume, wap, count):
@@ -23,14 +51,32 @@ class IbConn(EWrapper, EClient):
         except Exception as e:
             print(e)
 
-    def error(self, id , error, error_code):
-        print(id)
-        print(error)
-        print(error_code)
+class Bar:
+    open = 0
+    high = 0
+    low = 0
+    volume = 0
+    close = 0
+    date = ''
+
+    def __init__(self):
+        self.open = 0
+        self.high = 0
+        self.low = 0
+        self.volume = 0
+        self.close = 0
+        self.date = 0
 
 
 class Bot:
     ib = None
+    bar_size = 1
+    curr_bar = Bar()
+    reqId = 1
+    global order_id
+    smaPeriod = 50
+    symbol = ""
+    initial_bartime = datetime.datetime.now()
 
     # Connect to IB
     def __init__(self):
@@ -41,10 +87,18 @@ class Bot:
         ib_thread = threading.Thread(target=self.bot_loop(), daemon=True)
         ib_thread.start()
         time.sleep(1)
-        symbol = input("Enter the symbol you want to trade: ")
+        curr_bar = Bar()
+        self.bar_size = int(input("Enter bar size in minutes"))
+        self.symbol = input("Enter the symbol you want to trade: ")
+        min_text = ""
+        if self.bar_size > 1:
+            min_text = 'mins'
+        else:
+            min_text = 'min'
+        queryTime = datetime.datetime.now() - datetime.timedelta(days=1)
         # Creating IB Contract Objects
         contract = Contract()
-        contract.symbol = symbol.upper()
+        contract.symbol = self.symbol.upper()
         contract.secType = "STK"
         contract.exchange = "SMART"
         contract.currency = "USD"
@@ -69,9 +123,6 @@ class Bot:
         #Place the order
         order_id = random.randint(0,9000)
         self.ib.placeOrder(order_id, contract, order)
-
-
-
 
     # separate run method to separate thread
     def bot_loop(self):
